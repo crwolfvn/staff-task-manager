a977d5daa710f84f4ed00a264879ce84bcc50260
crwolfvn <crwolfvn@gmail.com>
2026-06-04 06:14:23 +0000

    Fix custom domain deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/staff-task-manager/',
+  // Khi không dùng Customs domain
+  // base: '/staff-task-manager/',
+  
+  // Khi có dùng customs domain
+    base: "/",
+
+  
 })
